d8743a297198041693f47fced53a0016b9e8d44f
Mariem Neili <mariemneili7@gmail.com>
2022-10-05 23:34:25 +0100

    set the inventory service

--- inventory-microservice/inventory-service/src/main/java/com/tn/inventoryservice/InventoryServiceApplication.java
+++ inventory-microservice/inventory-service/src/main/java/com/tn/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.tn.inventoryservice;
 
+import com.tn.inventoryservice.entities.Product;
+import com.tn.inventoryservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -9,5 +13,19 @@ public class InventoryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
+
+	/*
+	@Bean
+	CommandLineRunner start(ProductRepository productRepository){
+		return args -> {
+			productRepository.save(new Product(null,"pc", 1500,100));
+			productRepository.save(new Product(null,"tv", 1800,500));
+			productRepository.save(new Product(null,"phone", 780,100));
+			productRepository.findAll().forEach(p-> System.out.println(p.toString()));
+		};
+	}
+
+	 */
+
 
 }

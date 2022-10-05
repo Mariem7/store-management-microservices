4aa7dacfb88062d4647d050ec236c374d5f7924f
Mariem Neili <mariemneili7@gmail.com>
2022-10-05 20:53:32 +0100

    init customer service repository

---
+++ customer-microservice/customer-service/src/test/java/com/tn/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tn.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

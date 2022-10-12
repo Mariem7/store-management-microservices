648d0e914f0d6aad098433ecc8665d394dff5b24
Mariem Neili <mariemneili7@gmail.com>
2022-10-13 00:35:31 +0100

    config eureka discovery client

--- eureka-discovery-microservice/eureka-discovery/src/main/java/com/tn/eurekadiscovery/EurekaDiscoveryApplication.java
+++ eureka-discovery-microservice/eureka-discovery/src/main/java/com/tn/eurekadiscovery/EurekaDiscoveryApplication.java
@@ -2,8 +2,11 @@ package com.tn.eurekadiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+//active eureka server
+@EnableEurekaServer
 public class EurekaDiscoveryApplication {
 
 	public static void main(String[] args) {

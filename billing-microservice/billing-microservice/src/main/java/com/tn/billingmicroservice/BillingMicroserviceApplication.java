f23e5eb262a3a7eda882ad7d692d206feaa98aff
Mariem Neili <mariemneili7@gmail.com>
2022-10-13 00:43:09 +0100

    init billing microservice

---
+++ billing-microservice/billing-microservice/src/main/java/com/tn/billingmicroservice/BillingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tn.billingmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingMicroserviceApplication.class, args);
+	}
+
+}
